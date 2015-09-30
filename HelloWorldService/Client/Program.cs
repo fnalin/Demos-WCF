4af0b8ff2e0a510f29475757ff8a5ac8e31db2bb
Fabiano Nalin <fnalin@msn.com>
2015-09-29 23:48:37 -0300

    Add projeto HelloWorldService

---
+++ HelloWorldService/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string retorno;
+
+            using (var _proxy = new Servico.HelloWorldClient("WSHttpBinding_IHelloWorld"))
+            {
+                var person = new Servico.Name { First="Fabiano", Last="Teste do WSHttp"};
+                retorno = _proxy.SayHello(person);
+            }
+
+            Console.WriteLine(retorno);
+
+
+            Console.WriteLine("FIM");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorldService/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string First { get; set; }
+        [DataMember]
+        public string Last { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+
+    public class HelloWorldService : IHelloWorld
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1}", person.First, person.Last);
+        }
+    }
+}
