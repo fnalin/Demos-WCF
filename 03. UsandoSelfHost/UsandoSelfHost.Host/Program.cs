a30011e9feef6e3d361e36c5a1ca1fc6243eff67
Fabiano Nalin <fnalin@msn.com>
2015-10-03 15:45:47 -0300

    Add demo 3

---
+++ 03. UsandoSelfHost/UsandoSelfHost.Host/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using UsandoSelfHost.Service;
+
+namespace UsandoSelfHost.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1. Instanciar o servicehost passando o servico
+            var host = new ServiceHost(typeof(ClienteService)
+                //
+                //,new Uri[]
+                //  {
+                //      new Uri("net.tcp://localhost:8081"),
+                //      new Uri("http://localhost:8076/cliserv")
+                //  }
+            );
+
+            // Criar EndPoints explicitamente
+
+            //host.AddServiceEndpoint(typeof(IClienteService),
+            //    new BasicHttpBinding(),
+            //    "basic"
+            //);
+
+            //host.AddServiceEndpoint(typeof(IClienteService),
+            //    new WSHttpBinding(),
+            //    "ws"
+            //);
+
+            //host.AddServiceEndpoint(typeof(IClienteService),
+            //    new NetTcpBinding(),
+            //    "dados"
+            //);
+
+
+            //host.Description.Behaviors.Add(new ServiceMetadataBehavior() { HttpGetEnabled = true });
+            //host.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "meta");
+
+
+            //Disponibilizando o serviço
+            try
+            {
+                host.Open();
+                PrintServiceInfo(host);
+                Console.WriteLine("\nPressione enter para encerrar");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                host.Abort();
+            }
+
+        }
+
+        private static void PrintServiceInfo(ServiceHost host)
+        {
+            Console.WriteLine("{0} está online e disponível nesses endpoints:\n",
+                host.Description.ServiceType);
+
+            foreach (var se in host.Description.Endpoints)
+            {
+                Console.WriteLine(se.Address);    
+            }
+        }
+    }
+}
---
+++ 03. UsandoSelfHost/UsandoSelfHost.Service/ClienteLibrary.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace UsandoSelfHost.Service
+{
+
+    [DataContract]
+    public class Cliente
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Nome { get; set; }
+        [DataMember]
+        public int Idade { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IClienteService
+    {
+        [OperationContract]
+        void AddCliente(Cliente cli);
+        [OperationContract]
+        IList<Cliente> GetClientes();
+    }
+
+     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ClienteService : IClienteService
+    {
+        private readonly IList<Cliente> _clientes = new List<Cliente>();
+
+        public void AddCliente(Cliente cli)
+        {
+            _clientes.Add(cli);
+        }
+
+        public IList<Cliente> GetClientes()
+        {
+            return _clientes;
+        }
+    }
+}
