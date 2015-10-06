f31660ccbebba3ef1fce9321af36a5e777846265
Fabiano Nalin <fnalin@msn.com>
2015-10-05 23:32:53 -0300

    Add cliente com channel no cliente --> Demo 4

---
+++ 04. DemosClient/DemosClient.ClienteViaChannel/Program.cs
@@ -0,0 +1,33 @@
+﻿using DemosClient.ClienteViaChannel.AniversarianteServiceReference;
+using System;
+using System.Linq;
+using System.ServiceModel;
+
+namespace DemosClient.ClienteViaChannel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ChannelFactory<IAniversarianteServiceChannel> cf =
+                new ChannelFactory<IAniversarianteServiceChannel>("BasicHttpBinding_IAniversarianteService");
+
+            IAniversarianteServiceChannel channel = cf.CreateChannel();
+
+            var aniversariante = new Aniversariante { Nome = "José da Silva",Data=new DateTime(1999,08,20)};
+            channel.AddAniversariante(aniversariante);
+            channel.AddAniversariante(aniversariante);
+            channel.AddAniversariante(aniversariante);
+
+            var aniversariantes = channel.GetAniversariantes();
+            aniversariantes.ToList().ForEach(d => { Console.WriteLine("Nome: {0} - Data: {1}",d.Nome,d.Data.ToShortDateString()); });
+
+
+            channel.Close();
+
+
+
+        }
+    }
+}
