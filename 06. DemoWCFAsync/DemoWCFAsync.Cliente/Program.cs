6c0a43a63179e6c0a0b002a4cc3367e2d566cb92
Fabiano Nalin <fnalin@msn.com>
2015-10-06 23:38:46 -0300

    Add Demo 6

---
+++ 06. DemoWCFAsync/DemoWCFAsync.Cliente/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWCFAsync.Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestandoRetorno();
+            Console.WriteLine("\n\nusando Async agora\n\n");
+            TestandoRetornoAsync();
+            Console.WriteLine("Fim - acabou? tem certeza?");
+            Console.ReadLine();
+        }
+
+        private static void TestandoRetorno()
+        {
+            using (var _proxy = new ServiceReference.ServiceTestClient())
+            {
+                Console.WriteLine("Indo buscar as informações no server");
+                Console.WriteLine("Retorno do Método: {0}",_proxy.Retorno());
+                Console.WriteLine("Retornando do server");
+            }
+        }
+
+        private static async void TestandoRetornoAsync() //não esquecer do async na assinatura
+        {
+            using (var _proxy = new ServiceReference.ServiceTestClient())
+            {
+                Console.WriteLine("Indo buscar as informações no server");
+                var result = await _proxy.RetornoAsync();
+                Console.WriteLine("Retorno do Método: {0}", result);
+                Console.WriteLine("Retornando do server");
+
+            }
+        }
+    }
+}
---
+++ 06. DemoWCFAsync/DemoWCFAsync.Service/LibService.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceModel;
+
+namespace DemoWCFAsync.Service
+{
+    [ServiceContract]
+    public interface IServiceTest
+    {
+        [OperationContract]
+        string Retorno();
+    }
+
+    public class ServiceTest : IServiceTest
+    {
+
+        public string Retorno()
+        {
+            //simulando um processamento de 5 segundos
+            System.Threading.Thread.Sleep(5000);
+            return "oieee... retornei";
+        }
+    }
+}
