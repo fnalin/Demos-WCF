52128df8f467ee172f4c8164f9aef697357811f1
Fabiano Nalin <fnalin@msn.com>
2015-10-20 23:57:36 -0200

    Add demo service app

---
+++ 03.02 UsandoWcfServiceApplicationTemplate/WcfServiceApplicationTemplate/IMathService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace WcfServiceApplicationTemplate
+{
+    [ServiceContract]
+    public interface IMathService
+    {
+        [OperationContract]
+        int Add(int n1, int n2);
+
+        [OperationContract]
+        int Substract(int n1, int n2);
+
+        [OperationContract]
+        int Multiply(int n1, int n2);
+
+        [OperationContract]
+        int Divide(int n1, int n2);
+    }
+}
---
+++ 03.02 UsandoWcfServiceApplicationTemplate/WcfServiceApplicationTemplate/MathService.svc.cs
@@ -0,0 +1,25 @@
+﻿namespace WcfServiceApplicationTemplate
+{
+    public class MathService : IMathService
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Divide(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+
+        public int Multiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public int Substract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
